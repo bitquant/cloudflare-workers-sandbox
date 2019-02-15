e2c32b2231422ee869870786df065e36b27fe140
bitquant <bitquant@bitquant.com>
2019-02-15 17:51:07 -0600

    request and fetch dependency logging

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # cloudflare-workers-sandbox
 
-Test your Cloudflare Workers application with your own local machine.  This package configures a sandbox Cloudflare environment suitable for running and testing your worker code locally.
+Test your Cloudflare Workers application with your local machine.  This package configures a sandbox Cloudflare environment suitable for running and testing your worker code locally.
 
 ## Install
 ```
@@ -13,6 +13,9 @@ npx cloudflare-workers-sandbox /path/to/cloudflare/workers/app.js
 
 curl http://localhost:3000
 ```
+
+## Request Tracing
+The sandbox logs all requests received and all fetch() dependencies made during request processing.
 
 ## License
 MIT license; see [LICENSE](./LICENSE).
--- bin/cli.js
+++ bin/cli.js
@@ -12,8 +12,33 @@ var btoa = require('btoa');
 var atob = require('atob');
 var Crypto = require('node-webcrypto-ossl');
 
+var colors = require('colors');
+
+var fetchLog = function(...args) {
+
+    const startTime = new Date();
+
+    return fetch(...args).then(function(result) {
+
+        const endTime = new Date();
+        const responseTime = endTime - startTime;
+
+        var status = '' + result.status;
+
+        status = status >= 500 ? status.red
+            : status >= 400 ? status.yellow
+            : status >= 300 ? status.cyan
+            : status >= 200 ? status.green
+            : status.blue;
+
+        console.log(`${'<fetch>'.magenta} ${args[0]} ${status} ${responseTime} ms`);
+
+        return result;
+    })
+}
+
 var sandbox = {
-    fetch, Request, Response, Headers, URL,
+    fetch: fetchLog, Request, Response, Headers, URL,
     TextEncoder, TextDecoder, btoa, atob,
     crypto: new Crypto(),
     console, // share console with sandbox
@@ -31,9 +56,15 @@ var vm = require('vm');
 vm.createContext(sandbox);
 vm.runInContext(worker, sandbox);
 
-var http = require('http')
-var port = 3000
-var server = http.createServer(handler);
+var http = require('http');
+var morgan = require('morgan');
+var logger = morgan('dev');
+var port = 3000;
+var server = http.createServer(function(req, res) {
+    logger(req, res, function() {
+        handler(req, res);
+    })
+});
 server.on('listening', () => console.log(`Cloudflare Workers Sandbox: ready on port ${port}`));
 server.listen(port);
 
