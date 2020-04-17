6a285d92ac45594c1e34e546c44e1f05ebb73ecb
bitquant <bitquant@bitquant.com>
2020-04-16 22:36:17 -0500

    log fetch requests that throw errors

--- bin/cli.js
+++ bin/cli.js
@@ -42,6 +42,24 @@ var fetchLog = function(...args) {
         console.log(`${'<fetch>'.magenta} ${method.cyan} ${url} ${status} ${responseTime} ms`);
 
         return result;
+    }).catch(function(err) {
+
+        const endTime = new Date();
+        const responseTime = endTime - startTime;
+
+        var status = 'ERROR'.red;
+
+        let method = 'GET'
+        let options = args[1];
+        if (options && options.method) {
+            method = options.method;
+        }
+
+        let url = (typeof args[0] === 'string') ? args[0] : args[0].url;
+        let msg = `${err}`;
+        console.log(`${'<fetch>'.magenta} ${method.cyan} ${url} ${status} ${responseTime} ms ${msg.brightMagenta}`);
+
+        throw err;
     })
 }
 
