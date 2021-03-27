2d0aa720c1d4a29a077a697fa10635f06bd0de05
bitquant <bitquant@bitquant.com>
2021-03-26 22:45:04 -0500

    timer and instanceof support

--- bin/cli.js
+++ bin/cli.js
@@ -63,16 +63,93 @@ var fetchLog = function(...args) {
     })
 }
 
-var sandbox = {
-    cloudflareWorkersSandbox: true,
-    fetch: fetchLog, Request, Response, Headers, URL,
-    TextEncoder, TextDecoder, btoa, atob,
-    crypto: new Crypto(),
-    caches, // dummy cache
-    console, // share console with sandbox
-    addEventListener: (event, listener) => { eventListener = listener }
-}
+class Context {
 
+    constructor() {
+        this.cloudflareWorkersSandbox = true
+        this.addEventListener = (event, listener) => { eventListener = listener }
+        this.setTimeout = setTimeout
+        this.clearTimeout = clearTimeout
+        this.setInterval = setInterval
+        this.clearInterval = clearInterval
+        this.fetch = fetchLog
+        this.Request = Request
+        this.Response = Response
+        this.Headers = Headers
+        this.URL = URL
+        //this.URLSearchParams = URLSearchParams
+        //this.ReadableStream = ReadableStream
+        //this.WritableStream = WritableStream
+        //this.TransformStream = TransformStream
+        //this.FetchEvent = FetchEvent
+        this.caches = caches // dummy cache
+        this.crypto = new Crypto()
+        this.TextDecoder = TextDecoder
+        this.TextEncoder = TextEncoder
+        this.atob = atob
+        this.btoa = btoa
+
+        // These are necessary to use "instanceof" within a vm
+        this.Array = Array
+        this.ArrayBuffer = ArrayBuffer
+        this.Atomics = Atomics
+        this.BigInt = BigInt
+        this.BigInt64Array = BigInt64Array
+        this.BigUint64Array = BigUint64Array
+        this.Boolean = Boolean
+        this.DataView = DataView
+        this.Date = Date
+        this.Error = Error
+        this.EvalError = EvalError
+        this.Float32Array = Float32Array
+        this.Float64Array = Float64Array
+        this.Function = Function
+        this.Int8Array = Int8Array
+        this.Int16Array = Int16Array
+        this.Int32Array = Int32Array
+        this.Intl = Intl
+        this.JSON = JSON
+        this.Map = Map
+        this.Math = Math
+        this.NaN = NaN
+        this.Number = Number
+        this.Object = Object
+        this.Promise = Promise
+        this.Proxy = Proxy
+        this.RangeError = RangeError
+        this.ReferenceError = ReferenceError
+        this.Reflect = Reflect
+        this.RegExp = RegExp
+        this.Set = Set
+        this.SharedArrayBuffer = SharedArrayBuffer
+        this.String = String
+        this.Symbol = Symbol
+        this.SyntaxError = SyntaxError
+        this.TypeError = TypeError
+        this.URIError = URIError
+        this.Uint8Array = Uint8Array
+        this.Uint8ClampedArray = Uint8ClampedArray
+        this.Uint16Array = Uint16Array
+        this.Uint32Array = Uint32Array
+        this.WeakMap = WeakMap
+        this.WebAssembly = WebAssembly
+        this.console = console
+        this.constructor = constructor
+        this.decodeURI = decodeURI
+        this.decodeURIComponent = decodeURIComponent
+        this.encodeURI = encodeURI
+        this.encodeURIComponent = encodeURIComponent
+        this.escape = escape
+        this.globalThis = this
+        this.isFinite = isFinite
+        this.isNaN = isNaN
+        this.parseFloat = parseFloat
+        this.parseInt = parseInt
+        this.self = this
+        this.undefined = undefined
+        this.unescape = unescape
+    }
+}
 
 if (process.argv.length <= 2) {
     throw new Error('worker script path missing');
@@ -80,6 +157,7 @@ if (process.argv.length <= 2) {
 var fs = require('fs');
 var worker = fs.readFileSync(process.argv[2], 'utf8');
 var eventListener;
+var sandbox = new Context();
 var vm = require('vm');
 vm.createContext(sandbox);
 vm.runInContext(worker, sandbox);
